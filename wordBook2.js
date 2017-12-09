1f30f1cdabc0dd12dc2a8f839f3d990f8d4b164f
DCWorks <pshonature@gmail.com>
2017-12-09 10:17:36 +0900

    Start
    처음 시작함.

---
+++ wordBook2.js
@@ -0,0 +1,28 @@
+let wordBook_2017_002 =
+{ "title": "감정 표현 단어",
+  "name": "wordBook_2017_002",
+  "file": "wordBook2.js",
+  "style": "Word Pronunciation Memo",
+  "words": [
+              {"word":"agitate", "pronunciation":"ǽdʒitèit", "memo":"선동하다, 흥분시키다, 휘젓다"},
+              {"word":"amaze", "pronunciation":"əméiz", "memo":"놀랍다, 멋진, 대단한 , 굉장한 , 훌륭한"},
+              {"word":"annoy", "pronunciation":"ənɔ́i", "memo":"짜증나다, 성가시다, 화가 나다, 귀찮다, 불쾌하다"},
+              {"word":"bewilder", "pronunciation":"biwíldər", "memo":"당황하게하다, 당황케 하다, 어쩔 줄 모르게 하다"},
+              {"word":"bore", "pronunciation":"bɔːr", "memo":"지루하게 하다, 따분하게 하다, 땅굴을 파다, 구멍을 뚫다"},
+              {"word":"depress", "pronunciation":"diprés", "memo":"우울하게 하다, 불경기로 만들다, 의기소침하게 하다"},
+              {"word":"disappoint", "pronunciation":"dìsəpɔ́int", "memo":"실망시키다, 낙담시키다, 기대에 어긋나다, 망치다"},
+              {"word":"discourage", "pronunciation":"diskə́ːridʒ", "memo":"낙담시키다, 단념시키다, 좌절시키다"},
+              {"word":"disturb", "pronunciation":"distə́ːrb", "memo":"방해하다, 혼란시키다, 교란하다, 어지럽히다, 불안하게 하다"},
+              {"word":"embarrass", "pronunciation":"imbǽrəs", "memo":"당황하다, 부끄럽게 하다, 난처하게 하다, 어리둥절하게 하다, 방해하다"},
+              {"word":"excite", "pronunciation":"iksáit", "memo":"흥분시키다, 신나게 하다, 들뜨게 하다, 기대하게 만들다, 흥미롭게 하다"},
+              {"word":"exhaust", "pronunciation":"igzɔ́ːst", "memo":"배기, 지치다, 피곤하다, 소모시키다, 고갈"},
+              {"word":"fascinate", "pronunciation":"fǽsənèit", "memo":"매료시키다, 사로잡다, 관심을 보이다, 매혹시키다"},
+              {"word":"frighten", "pronunciation":"fráitn", "memo":"무섭게하다, 두렵게하다, 놀라게하다, 겁주다, 공포"},
+              {"word":"frustrate", "pronunciation":"frΛstreit", "memo":"좌절시키다, 짜증나는, 실망시키다, 화나게 하다, 안타까운"},
+              {"word":"interest", "pronunciation":"íntərəst, -tərèst", "memo":"관심, 이자, 이해관계"},
+              {"word":"irritate", "pronunciation":"írətèit", "memo":"짜증나게 하다, 화나게 하다, 신경질나게 하다, 염증을 일으키다"},
+              {"word":"please", "pronunciation":"pliːz", "memo":"제발, 부디"},
+              {"word":"surprise", "pronunciation":"sərpráiz", "memo":"놀라다, 놀라게 하다, 깜짝, 놀람, 기습하다"},
+              {"word":"tempt", "pronunciation":"tempt", "memo":"유혹하다, 하고 싶은, 끌다, 꾀다"}
+    ]
+};
